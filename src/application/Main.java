071d1ce39e8acdfc9b3e2de88b5e897b12fb34b5
khj <khj@gmail.com>
2019-06-04 16:03:42 +0900

    0604 수정2

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			this.rootStage = primaryStage;
-	        this.rootStage.setTitle("FAITHINKER");
+	        this.rootStage.setTitle("FAITHINKEe");
 	        
 			loadMainBoderPane();
 			loadMemberAnchorPane();			
